ef7913db29f472393cd786745c969591a37f2dbc
kdh0324 <kdh0324@postech.ac.kr>
2021-03-24 17:00:16 +0900

    Add equation library

---
+++ Equation/equation.py
@@ -0,0 +1,18 @@
+class Equation:
+
+    def __init__(self):
+        self.variables = dict()
+        self.rhs = 0
+        self.type_can_changed = True
+
+    def add_variable(self):
+        self.variables[len(self.variables)] = 0
+
+    def set_type(self, _type):
+        self.type = _type
+
+    def set_coefficient(self, variable_index, coefficient):
+        self.variables[variable_index] = coefficient
+
+    def set_rhs(self, _rhs):
+        self.rhs = _rhs
